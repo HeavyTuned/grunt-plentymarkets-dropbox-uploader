0bcfafe5acf4bcae335844adff2a7afd967968d9
HTH2 <it@heavy-tuned.de>
2016-11-23 13:13:22 +0100

    fix for new grunt version & node >= 6

--- tasks/importer.js
+++ tasks/importer.js
@@ -1,10 +1,20 @@
-var PlentyCurlAPI = require("node-plentymarkets-curl");
 module.exports = function gruntTask(grunt) {
 	'use strict';
-	
+	/*
+		
+		commadn to check for "in arbeit" dropbox imports
+request:{"requests":[{"_dataName":"DropboxProcessingList", "_moduleName":"lib/dropbox", "_searchParams":{"source":"design/HTV3"}, "_writeParams":{}, "_validateParams":{}, "_commandStack":[{"type":"read", "command":"read"}], "_dataArray":{}, "_dataList":{}}], "meta":{"id":3, "token":"JLM44D53S4GMMUAD5GK2QDX0271020161136"}}
+
+
+command for publish layout
+
+request:{"requests":[{"_dataName":"TemplateDesign", "_moduleName":"cms/template/structure/design", "_searchParams":{}, "_writeParams":{"designName":"HTV3", "templates":true, "categories":false, "blogs":false}, "_validateParams":{}, "_commandStack":[{"type":"write", "command":"buildTemplateDesign"}], "_dataArray":{}, "_dataList":{}}], "meta":{"id":3, "token":"JLM44D53S4GMMUAD5GK2QDX0271020161136"}}
+
+*/
 	
 	grunt.registerMultiTask('plentymarkets-dropbox-uploader', 'Import your dropbox Layout into the plentymarkets backend', function() {
 		var options = this.data;
+		var PlentyCurlAPI = require("node-plentymarkets-curl");
 		var done = this.async();
 		var plenty = new PlentyCurlAPI();
 		if(!options.layoutName || !options.layoutLang){
@@ -12,7 +22,6 @@ module.exports = function gruntTask(grunt) {
 		}
 		plenty.setCreditials(options);
 		plenty.setDebug(false);
-
 
 		plenty.login(function(loginResult){
 			var requestString = 'request='+plenty.stringifyNestedObject({"requests":[{"_dataName":"TemplateImportTemplate", "_moduleName":"cms/template/import", "_searchParams":{}, "_writeParams":{"designName":options.layoutName, "lang":options.layoutLang, "importAll":false}, "_validateParams":{}, "_commandStack":[{"type":"write", "command":"writeFromDropbox"}], "_dataArray":{}, "_dataList":{}}], "meta":{"token":plenty.getUserToken(),"id":plenty.getUserID()}});
