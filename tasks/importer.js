1712b7fe9d80d1858868cecbba2cf187a90225da
HTH2 <it@heavy-tuned.de>
2017-04-26 07:51:28 +0200

    options.domain non requires either a full http or https:// path

--- tasks/importer.js
+++ tasks/importer.js
@@ -1,20 +1,20 @@
-module.exports = function gruntTask(grunt) {
+module.exports = function (grunt) {
 	'use strict';
 	/*
-		
-		commadn to check for "in arbeit" dropbox imports
-request:{"requests":[{"_dataName":"DropboxProcessingList", "_moduleName":"lib/dropbox", "_searchParams":{"source":"design/HTV3"}, "_writeParams":{}, "_validateParams":{}, "_commandStack":[{"type":"read", "command":"read"}], "_dataArray":{}, "_dataList":{}}], "meta":{"id":3, "token":"JLM44D53S4GMMUAD5GK2QDX0271020161136"}}
+	commadn to check for "in arbeit" dropbox imports
+	request:{"requests":[{"_dataName":"DropboxProcessingList", "_moduleName":"lib/dropbox", "_searchParams":{"source":"design/HTV3"}, "_writeParams":{}, "_validateParams":{}, "_commandStack":[{"type":"read", "command":"read"}], "_dataArray":{}, "_dataList":{}}], "meta":{"id":UserID, "token":"xyz"}}
 
 
-command for publish layout
+	command for publish layout
 
-request:{"requests":[{"_dataName":"TemplateDesign", "_moduleName":"cms/template/structure/design", "_searchParams":{}, "_writeParams":{"designName":"HTV3", "templates":true, "categories":false, "blogs":false}, "_validateParams":{}, "_commandStack":[{"type":"write", "command":"buildTemplateDesign"}], "_dataArray":{}, "_dataList":{}}], "meta":{"id":3, "token":"JLM44D53S4GMMUAD5GK2QDX0271020161136"}}
+	request:{"requests":[{"_dataName":"TemplateDesign", "_moduleName":"cms/template/structure/design", "_searchParams":{}, "_writeParams":{"designName":"HTV3", "templates":true, "categories":false, "blogs":false}, "_validateParams":{}, "_commandStack":[{"type":"write", "command":"buildTemplateDesign"}], "_dataArray":{}, "_dataList":{}}], "meta":{"id":UserID, "token":"xyz"}}
 
-*/
-	
+	*/
+
 	grunt.registerMultiTask('plentymarkets-dropbox-uploader', 'Import your dropbox Layout into the plentymarkets backend', function() {
-		var options = this.data;
 		var PlentyCurlAPI = require("node-plentymarkets-curl");
+		var options = this.data;
+
 		var done = this.async();
 		var plenty = new PlentyCurlAPI();
 		if(!options.layoutName || !options.layoutLang){
@@ -27,17 +27,15 @@ request:{"requests":[{"_dataName":"TemplateDesign", "_moduleName":"cms/template/
 			var requestString = 'request='+plenty.stringifyNestedObject({"requests":[{"_dataName":"TemplateImportTemplate", "_moduleName":"cms/template/import", "_searchParams":{}, "_writeParams":{"designName":options.layoutName, "lang":options.layoutLang, "importAll":false}, "_validateParams":{}, "_commandStack":[{"type":"write", "command":"writeFromDropbox"}], "_dataArray":{}, "_dataList":{}}], "meta":{"token":plenty.getUserToken(),"id":plenty.getUserID()}});
 
 			if(loginResult.success === true){
-
-				plenty.post("https://"+options.domain+"/plenty/api/ui.php",requestString, function(callResult){
+				plenty.post(options.domain+"/plenty/api/ui.php",requestString, function(callResult){
 					done();
-					
 				});
 			}else{
-
+				try{
+					grunt.log.error(loginResult.exception.message);
+				}catch(exception){} /*if no error object is passed*/
 			}
 		});
-
-
 
 	});
 };
